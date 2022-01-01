4d38e70a94169feef58e5941766daecc39cf3075
emiljiang <emil@jjey.com>
2022-01-01 13:04:43 -0500

    changed Drivetrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -5,32 +5,32 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class Drivetrain implements Subsystem{
+    //declaring the wheels
     public DcMotor leftFront;
     public DcMotor rightFront;
     public DcMotor leftBack;
     public DcMotor rightBack;
-    int mode;
+
     @Override
     public void init(LinearOpMode opMode) {
+        //this part is accessing the wheels from the hardware map?? letting program know which wheel to use??
         leftFront = opMode.hardwareMap.get(DcMotor.class, "lf");
         rightFront = opMode.hardwareMap.get(DcMotor.class, "rf");
-        leftBack = opMode.hardwareMap.get(DcMotor.class,  "lb");
+        leftBack = opMode.hardwareMap.get(DcMotor.class, "lb");
         rightBack = opMode.hardwareMap.get(DcMotor.class, "rb");
-//       intakeMotor = opMode.hardwareMap.get(DcMotor.class, "intake");
 
-        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        mode = 1;
-        if (mode == 0) {
-            leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-            rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-            leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-            rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        } else if(mode == 1) {
-            leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        }
+
+        //to make robot wheels spin the same way bc the motor inverts the other side's wheels
+        //reversing the one side of the robot's wheels direction
+        //bc the motor inverts the other side's wheels
+        //therefore being able to travel forward
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //to make robot stop when youre not moving the sticks
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 }
